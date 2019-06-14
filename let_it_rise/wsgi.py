23ee1d399611c7b77acfb88f9bb818fb94754eaf
Chris Williams <cwill833@gmail.com>
2019-06-14 11:46:10 -0500

    django app up and running

---
+++ let_it_rise/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for let_it_rise project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "let_it_rise.settings")
+
+application = get_wsgi_application()
