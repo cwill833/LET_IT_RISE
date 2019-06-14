e798c4b87bacd57a5f5f5f3fcc245529cc895484
Chris Williams <cwill833@gmail.com>
2019-06-14 11:53:26 -0500

    add datebase and ran migrate

--- let_it_rise/settings.py
+++ let_it_rise/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'let_it_rise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'let_it_rise',
     }
 }
 
